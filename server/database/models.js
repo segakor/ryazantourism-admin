b86bfc86a9f5c9bb78f879334776e6cbf3f67efa
Sergey Kornienko <SDKornienko@inno.local>
2024-08-24 12:11:02 +0300

    Update models.js

--- server/database/models.js
+++ server/database/models.js
@@ -1,10 +1,14 @@
 const sequelize = require("./db");
 const { DataTypes } = require("sequelize");
 
-const User = sequelize.define("users", {
-  userName: { type: DataTypes.STRING, primaryKey: true, unique: true },
-  password: { type: DataTypes.STRING },
-});
+const User = sequelize.define(
+  "users",
+  {
+    userName: { type: DataTypes.STRING, primaryKey: true, unique: true },
+    password: { type: DataTypes.STRING },
+  },
+  { underscored: true }
+);
 
 module.exports = {
   User,
